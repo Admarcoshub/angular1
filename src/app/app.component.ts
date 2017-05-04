e953d2207f7401b0c0300d16ccc5ceff4aa5ef30
Alumno02 s604 <a02s604@campustls.local>
2017-05-04 12:10:24 -0500

    subiendoa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>Hello </h1>
+  <app-header></app-header>
+  <app-footer></app-footer>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {  }
